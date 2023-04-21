dae4b7beba2a00c3775ac6615e78fb8be92bd1e9
Igor Ribeiro <110436075+igorcons852@users.noreply.github.com>
2023-04-21 20:33:46 -0300

    Update Questao02.java
    Correção e implementação de um codigo mais legivel e funcional.

--- src/provaEstagioAptareTecnologia/questao02/Questao02.java
+++ src/provaEstagioAptareTecnologia/questao02/Questao02.java
@@ -1,80 +1,95 @@
 package provaEstagioAptareTecnologia.questao02;
 
-	import java.util.ArrayList;
-	import java.util.Comparator;
-	import java.util.List;
-	import java.util.Scanner;
-	import java.util.stream.Collectors;
-	
-	/*
-		Implementar um programa que receba como parâmetro de entrada 4 números inteiros 
-		diversos e a ordem de exibi-los, crescente ou decrescentes. Além disso, informar 
-		quantos e quais números são pares e ímpares. Exibir também na mesma ordem 
-		definida pelo usuário. 
-			
-			Exemplo 1:
-	 		* Entrada 
-				Números: 10, 109, 864, 88
-				Ordem de Exibição: Crescente
-	 
-	 		* Saída
-				Números ordenados: 10, 88, 109, 864
-				Quantidade de Números Pares: 3
-				Exibindo Números Pares: 10, 88, 864
-				Quantidade de Números Ímpares: 1
-				Exibindo Números Ímpares: 109
-	 */
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
-	public class Questao02 {
-		enum Ordem {
-			CRESCENTE, DECRESCENTE;
-		}
+/*
+	Implementar um programa que receba como parâmetro de entrada 4 números inteiros 
+	diversos e a ordem de exibi-los, crescente ou decrescentes. Além disso, informar 
+	quantos e quais números são pares e ímpares. Exibir também na mesma ordem 
+	definida pelo usuário. 
+		
+		Exemplo 1:
+ 		* Entrada 
+			Números: 10, 109, 864, 88
+			Ordem de Exibição: Crescente
+ 
+ 		* Saída
+			Números ordenados: 10, 88, 109, 864
+			Quantidade de Números Pares: 3
+			Exibindo Números Pares: 10, 88, 864
+			Quantidade de Números Ímpares: 1
+			Exibindo Números Ímpares: 109
+ */
 
-		public static void ordena(List<Integer> numeros, Ordem ordem) {
-			if (ordem.equals(Ordem.CRESCENTE)) {
-				numeros.sort(Comparator.naturalOrder());
-			} else {
-				numeros.sort(Comparator.reverseOrder());
-			}
-			System.out.println("Números ordenados: " + numeros.toString().replace("[", "").replace("]", ""));
+public class Questao02 {
 
-		}
+	public final static int ORDEM_CRESCENTE = 1;
+	public final static int ORDEM_DECRESENTE = 2;
 
-		public static void quantidadeENumerosParesEImpares(List<Integer> numeros) {
-			List<Integer> numerosPares = numeros.stream().filter(numero -> numero % 2 == 0).collect(Collectors.toList());
-			List<Integer> numerosImpares = numeros.stream().filter(numero -> numero % 2 != 0).collect(Collectors.toList());
-
-			System.out.println("Quantidade de Números Pares: " + numerosPares.size());
-			System.out.println("Exibindo Números Pares: " + numerosPares.toString().replace("[", "").replace("]", ""));
-
-			System.out.println("Quantidade de Números Impares: " + numerosImpares.size());
-			System.out.println("Exibindo Números Impares: " + numerosImpares.toString().replace("[", "").replace("]", ""));
-		}
-
-		public static void main(String[] args) {
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Números: ");
-
-			String entradaNum = scan.nextLine();
-			String[] listEntrada = entradaNum.split(",");
-
-			List<Integer> numeros = new ArrayList<>();
-			for (String i : listEntrada) {
-				numeros.add(Integer.parseInt(i));
-			}
-			System.out.println("Ordem de Exibição: ");
-			String entrada = scan.next();
-			if (entrada.equalsIgnoreCase("Crescente")) {
-				ordena(numeros, Ordem.CRESCENTE);
-
-			} else if (entrada.equalsIgnoreCase("Decrescente")) {
-				ordena(numeros, Ordem.DECRESCENTE);
-			}
-
-			quantidadeENumerosParesEImpares(numeros);
-
-			scan.close();
-		}
-
+	// Aplicando ordem
+	public static void condicionalOrdem(String[] numeros, String ordem) {
+		ordenar(adicionarNalista(numeros), ordem.equalsIgnoreCase("CRESCENTE") ? ORDEM_CRESCENTE : ORDEM_DECRESENTE);
 	}
 
+	// Ordenando Lista
+	public static void ordenar(List<Integer> lista, Integer ordem) {
+		lista.sort(ordem == ORDEM_CRESCENTE ? Comparator.naturalOrder() : Comparator.reverseOrder());
+		System.out.println("Numeros ordenados : " + lista.toString().replace("[", "").replace("]", ""));
+	}
+
+	// Adicionando valores na Lista
+	public static List<Integer> adicionarNalista(String[] numeros) {
+		List<Integer> listaNumeros = new ArrayList<>();
+		for (String e : numeros) {
+			listaNumeros.add(Integer.parseInt(e));
+		}
+		return listaNumeros;
+	}
+
+	// Pares e Impares
+	public static void parImpar(List<Integer> lista) {
+		List<Integer> numerosPares = lista.stream().filter(numeros -> numeros % 2 == 0).collect(Collectors.toList());
+		List<Integer> numerosImpares = lista.stream().filter(numeros -> numeros % 2 != 0).collect(Collectors.toList());
+
+		imprimir("pares", numerosPares);
+		imprimir("impares", numerosImpares);
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite os numeros");
+
+		String entradaNumeros = scan.nextLine();
+		if (!entradaNumeros.matches("^\\d+(,\\d+)*$")) {
+			System.out.println("Entrada inválida. Digite uma lista de números separados por vírgulas.");
+			scan.close();
+
+		}
+		String[] numeros = entradaNumeros.split(",");
+
+		System.out.println("Qual a ordem ? ");
+		String entradaOrdem = scan.nextLine().trim();
+		if (!entradaOrdem.equalsIgnoreCase("CRESCENTE") && !entradaOrdem.equalsIgnoreCase("DECRESCENTE")) {
+			System.out.println("Ordem inválida. Digite 'CRESCENTE' ou 'DECRESCENTE'.");
+			scan.close();
+
+		}
+		condicionalOrdem(numeros, entradaOrdem);
+		parImpar(adicionarNalista(numeros));
+
+		scan.close();
+	}
+
+	// Imprimindo lista
+	public static void imprimir(String tipoNumeros, List<Integer> lista) {
+		System.out.println("Quantidade de numeros " + tipoNumeros + ":" + lista.size());
+		System.out.print("Numeros " + tipoNumeros + ":");
+		System.out.println(String.join(", ", lista.stream().map(String::valueOf).collect(Collectors.toList())));
+
+	}
+}
